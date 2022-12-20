2c5b4fb69ec1a01354b016f4130b5305a5064046
Joel Silva Uchoa <joeluchoa@gmail.com>
2022-12-21 00:01:11 +0100

    Update extractor.js

--- extractor.js
+++ extractor.js
@@ -1,47 +1,46 @@
-function crawl() {
-    var _export = "";
-    var header = 'isin, transaction_type, quantity, price, date';
-    var data = JSON.parse($( '#__NEXT_DATA__' )[0].innerHTML);
-    var data = data['props']['pageProps']['middlewareProps']['m5']['initialQueryResult'];
-    console.log(data);
-
-    jQuery.each(data, function(idx, val) {
-        if (idx.startsWith('BrokerSecurityTransactionSummary') && val['isCancellationRequested'] == false && val['finalisationReason'] == 'SETTLED') {
-            var type = val['cashTransactionType'];
-            var isin = val['security']['id'];
-            var quantity = data[ val['numberOfShares']['id'] ]['filled'];
-            var amount = val['totalAmount'];
-            var date = val['lastEventDateTime'];
-
-            _export += isin + ';' + type + ';' + quantity + ';' + amount/quantity + ';' + date.substring(0, 10) + "\n";
-        }
-        if (idx.startsWith('BrokerCashTransaction') && val['cashTransactionType'] == 'DISTRIBUTION') {
-            var type = val['type'];
-            var isin = val['security'];
-            var quantity = null;
-            var amount = val['amount'];
-            var date = val['lastEventDateTime'];
-            _export += isin + ';' + type + ";0;" + amount + ';' + date.substring(0, 10) + "\n";    
-        }
-    });
-
-    console.log(header + "\n" + _export);
-    return true;
-}
-
-if (!window.jQuery) {  
+function loadJQuery() {
     var file = document.createElement('script');
     file.setAttribute("type","text/javascript");
     file.setAttribute("src", "https://code.jquery.com/jquery-3.6.0.min.js");
     file.setAttribute("integrity", "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=");
     file.setAttribute("crossorigin", "anonymous");
     file.addEventListener('load', () => {
-    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
-    // use jQuery below
+        console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
     });
     document.getElementsByTagName('head')[0].appendChild(file);
-    console.log("Plugins werden geladen..."); 
-    setTimeout(() => { console.log("Plugin geladen!"); crawl(); }, 3000);
-} else {
-    crawl();
 }
+
+function extractTransaction(val) {
+    return {
+        id: val['id'],
+        amount: val['amount'] || 0.0,
+        currency: val['currency'] || '',
+        description: val['description'] || '',
+        dateTime: val['lastEventDateTime'] || '',
+        isin: val['relatedIsin'] || val['isin'] || '',
+        quantity: val['quantity'] || 0,
+        securityTransactionType: val['securityTransactionType'] || '',
+        status: val['status'] || 'CANCELLED',
+        type: val['side'] || val['cashTransactionType'] || ''
+    };
+}
+
+function crawl() {
+    var _export = "";
+    var data = JSON.parse($( '#__NEXT_DATA__' )[0].innerHTML);
+    var data = data['props']['pageProps']['middlewareProps']['m5']['initialQueryResult'];
+
+    jQuery.each(data, function(idx, val) {
+        var transaction = extractTransaction(val);
+        if (transaction.status == "SETTLED") {           
+             console.log(transaction);   
+        }
+    });
+
+    return true;
+}
+
+if (!window.jQuery) {  
+    loadJQuery();
+}
+setTimeout(() => { crawl(); }, 3000);
